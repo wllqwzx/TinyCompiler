5d57ba1b28ba07685f3eb0470c7db8eda43bf558
wll <iwanglulu@gmail.com>
2016-11-21 14:00:21 +0800

    update ast.ml

--- src/ast.ml
+++ src/ast.ml
@@ -1,16 +1,29 @@
+type basetype =
+  | Int_ty 
+  | Bool_ty
+  | Unit_ty
+
 type expression =
   | Const_exp     of int
-  | Diff_exp      of expression * expression
-  | Is_zero_exp   of expression
-  | If_exp        of expression * expression * expression
+  | Is_Zero_exp   of expression
+  | Sub_exp       of expression * expression
+  | Add_exp       of expression * expression
+  | Mul_exp       of expression * expression
+  | Div_exp       of expression * expression
   | Var_exp       of string
-  | Let_exp       of string * expression * expression
-  | Proc_exp      of string * expression
   | Call_exp      of expression * expression
-  | Begin_exp     of expression list
-  | Newref_exp    of expression
-  | Deref_exp     of expression
-  | Setref_exp    of expression * expression
 
+type statements =
+  | Empty_stat    
+  | Combine_stat  of statements * statements   
+  | Var_def_stat  of basetype * string * expression
+  | Var_set_stat  of string * expression 
+  | If_stat       of statements * statements
+  | While_stat    of expression * statements
+
+
+type function_def = 
+  | A_func of basetype * string * (string * basetype) list * statements * expression
+  
 type program =
-  | A_program of expression
+  | A_program of function_def
