41dee2a5121759f318b47e38579aba7090a7ed69
wll <iwanglulu@gmail.com>
2016-12-04 20:00:01 +0800

    update cfg.ml

--- src/cfg.ml
+++ src/cfg.ml
@@ -7,13 +7,29 @@ open Lexer
 open Ast
 open IrAst
 
-type cfg =
-    | 
+let funcHtb = Hashtbl.create ~hashable:String.hashable ()
 
+
+
+let transCommarrToHtb =
+    fun commarr ->
+    
+
+
+let makeFuncCFG = 
+    fun fundef ->
+    match fundef with
+    | Ir_a_function (name, params, commarr) -> Hashtbl.add funcHtb name (transCommarrToHtb commarr) 
+
+let rec makeFuncliCFG = 
+    fun fundefli ->
+    match fundefli with
+    | [] -> ()
+    | f::fl -> makeFuncCFG f; makeFuncliCFG fl 
 
 let makeCFG =
     fun irpgm -> 
     match irpgm with
-    | Ir_a_program fun_def_list -> 
+    | Ir_a_program fun_def_list -> makeFuncliCFG fun_def_list
 
 
